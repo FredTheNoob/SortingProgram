414aebb18d626da427d3935ea877e3b0eedf2f3d
FredTheNoob <frederikhammer02@gmail.com>
2021-02-15 13:06:25 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace BubbleSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSortTime = new System.Windows.Forms.Label();
+            this.btnBubbleSort = new MetroFramework.Controls.MetroButton();
+            this.lstGeneratedNumbers = new System.Windows.Forms.ListBox();
+            this.btnGenerateNumbers = new MetroFramework.Controls.MetroButton();
+            this.txtNumbers = new MetroFramework.Controls.MetroTextBox();
+            this.lstSortedNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblSortTime
+            // 
+            this.lblSortTime.AutoSize = true;
+            this.lblSortTime.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSortTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.lblSortTime.Location = new System.Drawing.Point(189, 342);
+            this.lblSortTime.Name = "lblSortTime";
+            this.lblSortTime.Size = new System.Drawing.Size(0, 17);
+            this.lblSortTime.TabIndex = 3;
+            // 
+            // btnBubbleSort
+            // 
+            this.btnBubbleSort.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnBubbleSort.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnBubbleSort.Location = new System.Drawing.Point(186, 413);
+            this.btnBubbleSort.Name = "btnBubbleSort";
+            this.btnBubbleSort.Size = new System.Drawing.Size(120, 58);
+            this.btnBubbleSort.Style = MetroFramework.MetroColorStyle.Red;
+            this.btnBubbleSort.TabIndex = 2;
+            this.btnBubbleSort.Text = "BubbleSort";
+            this.btnBubbleSort.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.btnBubbleSort.UseSelectable = true;
+            this.btnBubbleSort.Click += new System.EventHandler(this.btnBubbleSort_Click);
+            // 
+            // lstGeneratedNumbers
+            // 
+            this.lstGeneratedNumbers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.lstGeneratedNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstGeneratedNumbers.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstGeneratedNumbers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.lstGeneratedNumbers.FormattingEnabled = true;
+            this.lstGeneratedNumbers.Location = new System.Drawing.Point(60, 63);
+            this.lstGeneratedNumbers.Name = "lstGeneratedNumbers";
+            this.lstGeneratedNumbers.Size = new System.Drawing.Size(120, 275);
+            this.lstGeneratedNumbers.TabIndex = 5;
+            // 
+            // btnGenerateNumbers
+            // 
+            this.btnGenerateNumbers.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.btnGenerateNumbers.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnGenerateNumbers.Location = new System.Drawing.Point(60, 413);
+            this.btnGenerateNumbers.Name = "btnGenerateNumbers";
+            this.btnGenerateNumbers.Size = new System.Drawing.Size(120, 58);
+            this.btnGenerateNumbers.Style = MetroFramework.MetroColorStyle.Red;
+            this.btnGenerateNumbers.TabIndex = 1;
+            this.btnGenerateNumbers.Text = "Generate Numbers";
+            this.btnGenerateNumbers.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.btnGenerateNumbers.UseSelectable = true;
+            this.btnGenerateNumbers.Click += new System.EventHandler(this.btnGenerateNumbers_Click);
+            // 
+            // txtNumbers
+            // 
+            // 
+            // 
+            // 
+            this.txtNumbers.CustomButton.Image = null;
+            this.txtNumbers.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.txtNumbers.CustomButton.Name = "";
+            this.txtNumbers.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.txtNumbers.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtNumbers.CustomButton.TabIndex = 1;
+            this.txtNumbers.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtNumbers.CustomButton.UseSelectable = true;
+            this.txtNumbers.CustomButton.Visible = false;
+            this.txtNumbers.Lines = new string[] {
+        "10"};
+            this.txtNumbers.Location = new System.Drawing.Point(84, 384);
+            this.txtNumbers.MaxLength = 32767;
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.PasswordChar = '\0';
+            this.txtNumbers.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtNumbers.SelectedText = "";
+            this.txtNumbers.SelectionLength = 0;
+            this.txtNumbers.SelectionStart = 0;
+            this.txtNumbers.ShortcutsEnabled = true;
+            this.txtNumbers.Size = new System.Drawing.Size(75, 23);
+            this.txtNumbers.TabIndex = 0;
+            this.txtNumbers.Text = "10";
+            this.txtNumbers.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.txtNumbers.UseSelectable = true;
+            this.txtNumbers.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtNumbers.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // lstSortedNumbers
+            // 
+            this.lstSortedNumbers.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.lstSortedNumbers.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lstSortedNumbers.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstSortedNumbers.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.lstSortedNumbers.FormattingEnabled = true;
+            this.lstSortedNumbers.Location = new System.Drawing.Point(186, 63);
+            this.lstSortedNumbers.Name = "lstSortedNumbers";
+            this.lstSortedNumbers.Size = new System.Drawing.Size(120, 275);
+            this.lstSortedNumbers.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 477);
+            this.Controls.Add(this.lstSortedNumbers);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.btnGenerateNumbers);
+            this.Controls.Add(this.lstGeneratedNumbers);
+            this.Controls.Add(this.btnBubbleSort);
+            this.Controls.Add(this.lblSortTime);
+            this.ForeColor = System.Drawing.Color.White;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
+            this.Text = "Sorting Program";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.TransparencyKey = System.Drawing.Color.LightSlateGray;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSortTime;
+        private MetroFramework.Controls.MetroButton btnBubbleSort;
+        private System.Windows.Forms.ListBox lstGeneratedNumbers;
+        private MetroFramework.Controls.MetroButton btnGenerateNumbers;
+        private MetroFramework.Controls.MetroTextBox txtNumbers;
+        private System.Windows.Forms.ListBox lstSortedNumbers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace BubbleSort
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBubbleSort_Click(object sender, EventArgs e)
+        {
+            // Ryd alle items i listboxen, hvis der er nogen fra sidste klik
+            lstSortedNumbers.Items.Clear();
+
+            // Lav et int array som indeholder de items vi gerne vil sortere
+            int[] input = new int[lstGeneratedNumbers.Items.Count];
+            
+            // Fyld input arrayet op med et for loop
+            for (int i = 0; i < lstGeneratedNumbers.Items.Count; i++)
+            {
+                // Vi laver et cast og parser vores item til en int som vi forresten også skal lave til en string da listboxens items er en ObjectCollection
+                input[i] = int.Parse(lstGeneratedNumbers.Items[i].ToString());
+            }
+
+            // Disse variabler holder styr på hvor mange cycles/checks vi laver, 
+            // og derudover også hvor mange modifikationer vi foretager os (når vi flytter rundt på to tal)
+            double checks = 0;
+            double modifications = 0;
+
+            // Start et stopur ved brug af System.Diagnostics.Stopwatch biblioteket
+            Stopwatch watch = Stopwatch.StartNew();
+
+            // Dette nestede for loop sørger for at vi kører det hele igennem, hvis der kun var et for loop ville der kun foregå en iteration med modifikationer
+            for (int j = 0; j < input.Length; j++)
+            {
+                for (int i = 0; i < input.Length - 1 - j; i++)
+                {
+                    checks++;
+
+                    // Hvis det tal vi kigger på er større end det næste tal
+                    if (input[i] > input[i + 1])
+                    {
+                        // Vi skal nu lave en modifikation
+                        modifications++;
+                        // Byt da rundt på deres pladser
+                        int temp = input[i];
+                        input[i] = input[i + 1];
+                        input[i + 1] = temp;
+                    }
+                }
+            }
+
+            // Stop stopuret når vi er ude af det nestede for loop
+            watch.Stop();
+
+            // Fyld vores sorterede listbox op
+            foreach (int i in input)
+            {
+                lstSortedNumbers.Items.Add(i);
+            }
+
+            // Hvis statistik
+            lblSortTime.Text = $"{watch.ElapsedMilliseconds} ms\n{watch.ElapsedMilliseconds / Convert.ToDecimal(1000)} s\nChecks: {checks.ToString("#,#")}\nModifications: {modifications.ToString("#,#")}";
+        }
+
+        private void btnGenerateNumbers_Click(object sender, EventArgs e)
+        {
+            // Hvis det brugeren har indtastet ikke er et tal
+            if (!txtNumbers.Text.All(char.IsDigit)) {
+                // Bruger feedback
+                MetroMessageBox.Show(this.Owner ,"Invalid number format: the N input is only allowed to be a whole number excluding negative numbers, special characters, string letters, etc.", $"{txtNumbers.Text} - Error");
+                return; // Fortsæt ikke til koden nedenfor men hop ud af denne metode
+            }
+
+            // Nyt tilfældigt tal
+            Random rnd = new Random();
+            // Ryd vores liste af items hvis brugeren forsøger at generere nye tilfældige tal
+            lstGeneratedNumbers.Items.Clear();
+
+            // Fyld listboxen op med tilfældige tal -> vi caster vores text til en int så for loopet ved hvor langt det skal køre
+            for (int i = 0; i < Int32.Parse(txtNumbers.Text); i++)
+            {
+                // Vi tilføjer et tilfældige tal til listen mellem 0 og 999
+                lstGeneratedNumbers.Items.Add(rnd.Next(0,999));
+            }
+        }
+    }
+}
